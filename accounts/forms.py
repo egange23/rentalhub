662f06fe9a6b54bb8318bbbbc14cc1abb64696f0
Egange Ayuk <eayuk@email.com>
2023-06-28 19:49:46 +1000

    delay in hx-triggers

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,9 +15,9 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("phone","email")
         widgets = {
-            "username": forms.TextInput(attrs={'hx-post': '/accounts/check_username/', 'hx-trigger':'keyup', 'hx-target':'#hint_id_username',}),
-             "phone": forms.TextInput(attrs={'hx-post': '/accounts/check_phone/', 'hx-trigger':'keyup', 'hx-target':'#hint_id_phone',}),
-             "email": forms.TextInput(attrs={'hx-post':'/accounts/check_email/', 'hx-trigger':'keyup', 'hx-target':'#hint_id_email',}),
+            "username": forms.TextInput(attrs={'hx-post': '/accounts/check_username/', 'hx-trigger':'keyup changed delay:1000ms', 'hx-target':'#hint_id_username',}),
+             "phone": forms.TextInput(attrs={'hx-post': '/accounts/check_phone/', 'hx-trigger':'keyup changed delay:1000ms', 'hx-target':'#hint_id_phone',}),
+             "email": forms.TextInput(attrs={'hx-post':'/accounts/check_email/', 'hx-trigger':'keyup changed delay:1000ms', 'hx-target':'#hint_id_email',}),
                    }          
 
 class CustomUserChangeForm(UserChangeForm):
